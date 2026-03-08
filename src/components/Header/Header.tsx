80e72ea1defdc7be5cf4a276616d7b001ac9e2fc
Gabriel <gabrieldavis1901@gmail.com>
2026-03-07 22:53:08 -0800

    set up Boilerplate Headers

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,23 @@
+import { PropsWithChildren } from 'react';
+import styles from './Header.module.css'
+
+export interface HeaderProps extends PropsWithChildren {
+  as?: 'h1' | 'h2' | 'h3' | 'h4'
+}
+
+export const Header = ({
+  children,
+  as = 'h1',
+  ...props
+}: HeaderProps) => {
+  const Tag = as;
+  return (
+    <Tag
+      className={styles.root}
+      {...props}
+    >
+      {children}
+    </Tag>
+  );
+
+};
