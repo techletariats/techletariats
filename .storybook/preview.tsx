d7501ded2d43941abe4dd55834248dbe5fb352e6
Jade Allen Cook <jadeallencook@gmail.com>
2026-03-08 15:40:54 -0700

    chore(globals): added global css vars for storybook and next

---
+++ .storybook/preview.tsx
@@ -0,0 +1,31 @@
+import type { Preview } from '@storybook/nextjs-vite';
+// @ts-expect-error css import
+import '../src/styles/globals.css';
+// @ts-expect-error css import
+import './storybook.css';
+
+const preview: Preview = {
+  parameters: {
+    backgrounds: {
+      default: 'dark',
+      values: [
+        { name: 'dark', value: 'var(--color-background)' },
+      ],
+    },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+
+    a11y: {
+      // 'todo' - show a11y violations in the test UI only
+      // 'error' - fail CI on a11y violations
+      // 'off' - skip a11y checks entirely
+      test: 'todo'
+    }
+  },
+};
+
+export default preview;
