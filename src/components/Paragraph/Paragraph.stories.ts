75507e1cf3591913a773c87f3be43a7308b158e5
Gabriel <gabrieldavis1901@gmail.com>
2026-03-08 16:33:06 -0700

    chore(globals): Initial-css-input

--- src/components/Paragraph/Paragraph.stories.ts
+++ src/components/Paragraph/Paragraph.stories.ts
@@ -15,7 +15,7 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-    args: {
-        children: "Hello Comrade"
-    }
+  args: {
+    children: "Surround yourself with people who make you happy. People who make you laugh, who help you when you’re in need. People who genuinely care. They are the ones worth keeping in your life. Everyone else is just passing through.",
+  },
 };
