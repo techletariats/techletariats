1769dff09eb82317651198244183c06397d7b5e2
Abstraxision <abstraxision@gmail.com>
2026-03-07 23:04:34 -0800

    link boilerplate

---
+++ src/components/Link/Link.stories.ts
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from "@storybook/nextjs-vite";
+import { Button } from ".";
+
+const meta = {
+  title: "Components/Button",
+  component: Button,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    children: "Hello Comrade",
+  },
+};
---
+++ src/components/Link/Link.tsx
@@ -0,0 +1,20 @@
+import { ButtonHTMLAttributes } from 'react';
+import styles from './button.module.css'
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  onClick?: () => void;
+}
+
+export const Button = ({
+  children,
+  ...props
+}: ButtonProps) => {
+  return (
+    <button
+      type="button"
+      className={styles.root}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
