f7febc8b2c98c90f01cb6596c93502f280ae2021
Clayton Cook <claytonleonardcook+github@gmail.com>
2026-03-08 16:58:20 -0700

    reformat files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,9 @@
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import { includeIgnoreFile } from "@eslint/compat";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 import storybook from "eslint-plugin-storybook";
 import { defineConfig } from "eslint/config";
-import { includeIgnoreFile } from "@eslint/compat";
 import { fileURLToPath } from "node:url";
 
 const gitignorePath = fileURLToPath(new URL(".gitignore", import.meta.url));
