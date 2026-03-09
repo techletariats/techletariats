d06b966913bd3340a6d54a7576ad86d3ba2a6eeb
Clayton Cook <claytonleonardcook+github@gmail.com>
2026-03-08 17:04:28 -0700

    reformat all files

--- src/components/Link/Link.stories.ts
+++ src/components/Link/Link.stories.ts
@@ -15,8 +15,8 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-  args: {
-    children: "Hello comrade",
-    href: "#",
-  },
+    args: {
+        children: "Hello comrade",
+        href: "#"
+    }
 };
--- src/components/Paragraph/Paragraph.stories.ts
+++ src/components/Paragraph/Paragraph.stories.ts
@@ -15,7 +15,8 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-  args: {
-    children: "Surround yourself with people who make you happy. People who make you laugh, who help you when you’re in need. People who genuinely care. They are the ones worth keeping in your life. Everyone else is just passing through.",
-  },
+    args: {
+        children:
+            "Surround yourself with people who make you happy. People who make you laugh, who help you when you’re in need. People who genuinely care. They are the ones worth keeping in your life. Everyone else is just passing through."
+    }
 };
